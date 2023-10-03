5a8c230012e50eae8b3067b1e1eb69f48f2bba09
Jules Sorensen <julessorens@gmail.com>
2023-10-03 15:20:36 +0200

    feat: send avis

--- Controller/Controller_PageAccueil.php
+++ Controller/Controller_PageAccueil.php
@@ -2,6 +2,25 @@
 require_once(dirname(__DIR__) . "/Model/Bdd.php");
 
 $bdd= new Bdd();
+
+if (isset($_POST['commentaire'])) {
+  $sendAvisErr = false;
+  $sendAvisMessage = "";
+
+  if (empty($_POST['nom'])) {
+    $sendAvisErr = "Vous devez rentrer un nom !";
+  } else if (empty($_POST['note'])) {
+    $sendAvisErr = "Vous devez rentrer une note !";
+  }
+
+  try {
+    $sendAvis = $bdd->insertAvisClient($_POST['nom'], $_POST['commentaire'], $_POST['note']);
+    $sendAvisMessage = "Votre avis a bien été envoyé, merci 🙂 !";
+  } catch (\Throwable $th) {
+    $sendAvisErr = "Une erreur est survenue lors de l'envoi de votre avis !";
+  }
+}
+
 $avis=$bdd->getAvisClients();
 
 require_once(dirname(__DIR__) . "/View/View_PageAccueil.php");
--- View/View_PageAccueil.php
+++ View/View_PageAccueil.php
@@ -7,8 +7,8 @@
   <title>Accueil</title>
   <style>
     <?php include 'style/Accueil.css' ?><?php
-                                        foreach ($avis as $value) {
-                                          echo ".star-rating" . $value["Av_id"] . " {
+      foreach ($avis as $value) {
+        echo ".star-rating" . $value["Av_id"] . " {
             white-space: nowrap;
         }
         
@@ -16,17 +16,17 @@
             appearance: none;
         }
         
-        .star-rating" . $value["Av_id"] . " i {
+        .star-rating" . $value["Av_id"] . " svg {
             font-size: 1.2em;
             transition: 0.3s;
         }
         
-        .star-rating" . $value["Av_id"] . " label:has(~ :checked) i svg {
+        .star-rating" . $value["Av_id"] . " label:has(~ :checked) svg {
           text-shadow: 0 0 2px #ffffff, 0 0 10px #ffee58;
           filter: invert(8%) sepia(100%) saturate(6480%) hue-rotate(246deg) brightness(102%) contrast(143%);
         }\n\n";
-                                        }
-                                        ?>
+      }
+    ?>
   </style>
   <link rel="icon" href="View/Pictures/logo.png" />
 </head>
@@ -81,42 +81,42 @@
 
   <div id="clientRatesDiv">
     <h2>Avis clients</h2>
-    <p>Voici ce que les <?php echo count($avis); ?> avis de nos clients pensent de nous :</p>
+    <p>Voici ce que nos <?php echo count($avis); ?> avis de clients pensent de nous :</p>
     <div id="clientRates">
       <?php
       foreach ($avis as $value) {
         echo "<div id='clientRate'>
               <h4>" . $value["Av_nom"] . "</h4>
               <span class='star-rating" . $value["Av_id"] . "'>
-                  <label for='rate-1-" . $value["Av_id"] . "' style='--i:1'><i class='fa-solid fa-star'>
+                  <label for='rate-1-" . $value["Av_id"] . "' style='--svg:1'>
                           <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 576 512'>
                               <path d='M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z' />
                           </svg>
-                      </i></label>
+                      </label>
                   <input type='radio' name='rating' id='rate-1-" . $value["Av_id"] . "' value='1' disabled " . ($value["Av_note"] == 1 ? "checked" : "") . ">
-                  <label for='rate-2-" . $value["Av_id"] . "' style='--i:2'><i class='fa-solid fa-star'>
+                  <label for='rate-2-" . $value["Av_id"] . "' style='--svg:2'>
                           <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 576 512'>
                               <path d='M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z' />
                           </svg>
-                      </i></label>
+                      </label>
                   <input type='radio' name='rating' id='rate-2-" . $value["Av_id"] . "' value='2' disabled " . ($value["Av_note"] == 2 ? "checked" : "") . ">
-                  <label for='rate-3-" . $value["Av_id"] . "' style='--i:3'><i class='fa-solid fa-star'>
+                  <label for='rate-3-" . $value["Av_id"] . "' style='--svg:3'>
                           <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 576 512'>
                               <path d='M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z' />
                           </svg>
-                      </i></label>
+                      </label>
                   <input type='radio' name='rating' id='rate-3-" . $value["Av_id"] . "' value='3' disabled " . ($value["Av_note"] == 3 ? "checked" : "") . ">
-                  <label for='rate-4-" . $value["Av_id"] . "' style='--i:4'><i class='fa-solid fa-star'>
+                  <label for='rate-4-" . $value["Av_id"] . "' style='--svg:4'>
                           <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 576 512'>
                               <path d='M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z' />
                           </svg>
-                      </i></label>
+                      </label>
                   <input type='radio' name='rating' id='rate-4-" . $value["Av_id"] . "' value='4' disabled " . ($value["Av_note"] == 4 ? "checked" : "") . ">
-                  <label for='rate-5-" . $value["Av_id"] . "' style='--i:5'><i class='fa-solid fa-star'>
+                  <label for='rate-5-" . $value["Av_id"] . "' style='--svg:5'>
                           <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 576 512'>
                               <path d='M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z' />
                           </svg>
-                      </i></label>
+                      </label>
                   <input type='radio' name='rating' id='rate-5-" . $value["Av_id"] . "' value='5' disabled " . ($value["Av_note"] == 5 ? "checked" : "") . ">
                   <p>" . $value["Av_commentaire"] . "</p>
               </span>
@@ -125,6 +125,13 @@
       ?>
     </div>
     <h3>Votre avis nous intéresse !</h3>
+      <?php
+      if ($sendAvisErr) {
+        echo "<p class='error'>" . $sendAvisErr . "</p>";
+      } else if ($sendAvisMessage) {
+        echo "<p class='success'>" . $sendAvisMessage . "</p>";
+      }
+      ?>
     <form id="rateForm" method="POST" action="#">
       <label for="nom">Votre nom :</label>
       <input id="rateFormNameInput" type="text" id="nom" name="nom" required>
@@ -137,7 +144,7 @@
         <option value="1">1 étoile</option>
       </select>
       <label for="commentaire">Commentaire :</label>
-      <textarea id="rateFormCommentInput" name="commentaire" id="commentaire" cols="30" rows="10" required></textarea>
+      <textarea id="rateFormCommentInput" name="commentaire" id="commentaire" cols="100" rows="10"></textarea>
       <input type="submit" value="Envoyer">
     </form>
   </div>
