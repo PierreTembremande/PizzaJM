d5b808a1ec8ee515ffa31026d21bcfc26607b5ef
WarrenVermelle <80685095+WarrenVermelle@users.noreply.github.com>
2023-10-03 10:48:39 +0200

    feature/page_accueil

--- View/View_PageAccueil.php
+++ View/View_PageAccueil.php
@@ -4,15 +4,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/Accueil.css">
     <title>Accueil</title>
-    <style>
-        <?php include 'style/Accueil.css' ?>
-    </style>
 </head>
 
 <body>
-    <h1>Accueil test</h1>
+    <h1>Accueil</h1>
+    <nav class="navbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
 
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Acccueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">item</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">item</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link"href="#">item</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <main>contenu du site</main>
+
+    <footer>
+        <ul class="footer-list">
+            <li class="footer-item">An item</li>
+            <li class="footer-item">A second item</li>
+            <li class="footer-item">A third item</li>
+        </ul>
+    </footer>
 </body>
 
 </html>
