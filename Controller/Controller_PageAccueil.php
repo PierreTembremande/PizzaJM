9e2ed2b0945dc806d9d39bf3de303c733729c5ee
Pierre Tembremande <pierre.tembremande@gmail.com>
2023-10-03 10:05:46 +0200

    Initialisation Bdd

--- Controller/Controller_PageAccueil.php
+++ Controller/Controller_PageAccueil.php
@@ -2,6 +2,6 @@
 require_once(dirname(__DIR__) . "..\Model\Bdd.php");
 require_once(dirname(__DIR__) . "..\View\View_PageAccueil.php");
 
-// $bdd= new Bdd();
+$bdd= new Bdd();
 
 // $entrepots=$bdd->getNomsEntrepots();
---
+++ Model/Bdd.php
@@ -0,0 +1,66 @@
+<?php
+
+class Bdd
+{
+
+    private $bdd;
+
+    function __construct()
+    {
+
+        $dsn = 'mysql:dbname=pizzajm;host=127.0.0.1:3306';
+        $dbUser = 'root';
+        $dbPwd = '';
+
+        try {
+            $this->bdd = new PDO($dsn, $dbUser, $dbPwd);
+        } catch (PDOException $e) {
+
+            echo $e->getMessage();
+        }
+    }
+
+
+    function getNomsEntrepots()
+    {
+
+        $sql = "SELECT nom_entrepot FROM entrepots;";
+        $rq = $this->bdd->prepare($sql);
+        $rq->execute();
+        return $rq->fetchAll();
+    }
+
+    function getProduits()
+    {
+
+        $sql = "SELECT fk_produit as id,
+         concat(substr(nom_fournisseur,1,3),substr(nom_categorie,1,3), fk_produit) as reference, 
+         nom_produit AS nom, 
+         prix_ht_produit AS prixht,
+         description_produit AS description, 
+         categories.nom_categorie AS categorie, 
+         marques.nom_marque AS marque ,
+         stocks.*
+         FROM produits 
+         JOIN categories ON fk_categorie_produit= id_categories 
+         JOIN marques ON fk_marque_produit = id_marques
+         JOIN stocks  on fk_produit = id_produit 
+         JOIN fournisseurs on fk_fournisseur_produit = id_fournisseurs;";
+        $rq =  $this->bdd->prepare($sql);
+        $rq->execute();
+        return $rq->fetchAll();
+    }
+
+    function getVerifLogin($email, $mdp){
+
+        $sql= "SELECT role_libelle
+                FROM users
+                JOIN roles ON idroles= fk_role
+                WHERE email_user=:email and password_user=:mdp";
+        $re = $this->bdd->prepare($sql);
+        $re->execute([":email" => $email, ":mdp"=>$mdp]);
+        return $re->fetch();
+
+    }
+
+}
