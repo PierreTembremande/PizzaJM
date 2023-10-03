30cacdd23732cb0ce145edf0993d82a7de4929a5
WarrenVermelle <warren.vermelle@outlook.com>
2023-10-03 10:57:09 +0200

    feature/page_accueil

--- View/View_PageAccueil.php
+++ View/View_PageAccueil.php
@@ -1,49 +1,35 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/Accueil.css">
-    <title>Accueil</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style/Accueil.css">
+        <title>Accueil</title>
+    </head>
 
-<body>
-    <h1>Accueil</h1>
-    <nav class="navbar">
-        <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
+    <body>
+        <h1>Pizza JM</h1>
+        <nav class="navbar">
+            <div class="container">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Notre pizzeria</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nos pizzas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Avis</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Acccueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">item</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">item</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link"href="#">item</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-    <main>contenu du site</main>
-
-    <footer>
-        <ul class="footer-list">
-            <li class="footer-item">An item</li>
-            <li class="footer-item">A second item</li>
-            <li class="footer-item">A third item</li>
-        </ul>
-    </footer>
-</body>
+        <main>Présentation de l'entreprise</main>
+    </body>
 
 </html>
